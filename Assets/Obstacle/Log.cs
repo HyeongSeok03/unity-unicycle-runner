abf10dc975fa68281bfe2eb65d125a482a789ce1
ZiLA3 <93520847+ZiLA3@users.noreply.github.com>
2025-12-05 01:55:10 +0900

    feat: 통나무

---
+++ Assets/Obstacle/Log.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Log : Obstacle
+{
+    [SerializeField] private float rotationTorque = 3f;
+    [SerializeField] private float springForce = 10f;
+    
+    protected override void Hit(Unicycle player)
+    {
+        var dz = Random.Range(-rotationTorque, rotationTorque);
+        var torque = new Vector3(rotationTorque, 0f, dz);
+        var worldTorque = transform.TransformDirection(torque);
+        
+        player.rb.AddTorque(worldTorque, ForceMode.Impulse);
+        player.rb.linearVelocity = new Vector3(0f, springForce, 0f);
+    }
+}
