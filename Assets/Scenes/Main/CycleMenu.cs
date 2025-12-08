04d477c6d8a77903b10484b6d788544fbdb2e305
ZiLA3 <93520847+ZiLA3@users.noreply.github.com>
2025-12-08 13:42:32 +0900

    feat: 메인화면 기초

---
+++ Assets/Scenes/Main/CycleMenu.cs
@@ -0,0 +1,15 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private Quaternion _startRotation;
+    private void Start()
+    {
+        _startRotation = transform.rotation;
+        var targetRotation = _startRotation.eulerAngles + new Vector3(0, 360, 0);
+        transform.DORotate(targetRotation, 20f, RotateMode.FastBeyond360)
+            .SetLoops(-1)
+            .SetEase(Ease.Linear);
+    }
+}
